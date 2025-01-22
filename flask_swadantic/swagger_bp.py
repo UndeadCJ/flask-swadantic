52c68c5fdc842e278dee23ac4aafea4dfd652236
Lucas de Freitas Ponick <lucas_ponick@hotmail.com>
2025-01-21 21:18:56 -0300

    Update file path resolution in Swagger Blueprint
    Improved file path handling by using `os.path.join` to resolve paths relative to the blueprint's root. This ensures more robust and platform-independent handling of template and static file locations.

--- flask_swadantic/swagger_bp.py
+++ flask_swadantic/swagger_bp.py
@@ -1,12 +1,17 @@
+import os
+
 from flask import Blueprint, send_file, send_from_directory
 
 swagger_bp = Blueprint("swagger_bp", __name__)
 
+
 @swagger_bp.get("")
 def get_swagger():
-    return send_file("templates/index.html")
+    filepath = os.path.join(swagger_bp.root_path, "templates", "index.html")
+    return send_file(filepath)
 
 
 @swagger_bp.get("/static")
 def get_swagger_static():
-    return send_from_directory("static", "/static")
+    static_folder = os.path.join(swagger_bp.root_path, "static")
+    return send_from_directory(static_folder, "/static")
